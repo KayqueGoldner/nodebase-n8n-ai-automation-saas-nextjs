fd2b664a85d7354c8dae018b4825b1bfd41e0374
Kayque Goldner <kayquegoldner@gmail.com>
2025-11-18 04:55:13 -0300

    feat: add dashboard layout with sidebar and routing

---
+++ src/app/(dashboard)/(rest)/credentials/page.tsx
@@ -0,0 +1,13 @@
+import { requireAuth } from "@/lib/auth-utils";
+
+const CredentialsPage = async () => {
+  await requireAuth();
+
+  return (
+    <div>
+      CredentialsPage
+    </div>
+  )
+}
+
+export default CredentialsPage;
